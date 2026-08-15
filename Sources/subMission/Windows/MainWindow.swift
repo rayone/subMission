c13b84a1de8d2a751d8a40ba006db327ff11336d
rayone <>
2026-08-15 20:56:48 +0800

    remove window.backgroundColor from main window (causes invisible table with .inset style)

--- Sources/subMission/Windows/MainWindow.swift
+++ Sources/subMission/Windows/MainWindow.swift
@@ -55,7 +55,6 @@ final class MainWindowController: NSWindowController, NSToolbarDelegate, NSWindo
         )
         window.title = "subMission"
         window.titleVisibility = .hidden
-        window.backgroundColor = Theme.bg
         window.minSize = NSSize(width: 640, height: 400)
         window.center()
         super.init(window: window)
